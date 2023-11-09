b7ceb79073a07bddbc116762c92af81ed1b08e7c
Myomyoring <narcissuscat@naver.com>
2023-11-09 23:11:17 +0900

    백준-2903번

---
+++ baekjoon/단계별/일반 수학 1/2903-중앙 이동 알고리즘.js
@@ -0,0 +1,6 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim();
+let dot = 2;
+for (let i = 0; i < input; i++) {
+	dot += Math.pow(2, i);
+}
+console.log(Math.pow(dot, 2));
